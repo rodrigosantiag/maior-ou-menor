1795048c43cffd6ed7e10489d76faabd125f3f7c
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-02-15 10:19:50 -0300

    refactoring

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,39 +1,48 @@
 # frozen_string_literal: true
 
-puts 'Bem vindo ao jogo da adivinhação'
+def da_boas_vindas
+  puts 'Bem vindo ao jogo da adivinhação'
+  puts 'Qual é o seu nome?'
+  nome = gets
+  puts "\n\n\n"
+  puts "Começaremos o jogo para você, #{nome}"
+end
 
-puts 'Qual é o seu nome?'
-nome = gets
+def escolhe_numero_secreto
+  puts 'Escolhendo um número secreto entre 0 e 200...'
+  sorteado = 175
+  puts 'Escolhido... que tal adivinhar hoje nosso número secreto?'
+  sorteado
+end
 
-puts "\n\n\n"
-
-puts "Começaremos o jogo para você, #{nome}"
-
-puts 'Escolhendo um número secreto entre 0 e 200...'
-puts 'Escolhido... que tal adivinhar hoje nosso número secreto?'
-numero_secreto = 175
-
-limite_de_tentativas = 5
-
-for tentativa in 1..limite_de_tentativas
+def pede_um_numero(tentativa, limite_de_tentativas)
   puts "\n\n"
-
   puts "Tentativa #{tentativa} de #{limite_de_tentativas}"
   puts 'Entre com o número'
   chute = gets
   puts "Será que acertou? Você chutou #{chute}"
+  chute.to_i
+end
 
-  acertou = chute.to_i == numero_secreto
+def verifica_se_acertou(numero_secreto, chute)
+  acertou = chute == numero_secreto
 
   if acertou
     puts 'Acertou!'
-    break
-  else
-    maior = numero_secreto > chute.to_i
-    if maior
-      puts 'Número escolhido é maior'
-    else
-      puts 'Número escolhido é menor'
-    end 
+    return true
   end
+  maior = numero_secreto > chute
+  puts maior ? 'Número escolhido é maior' : 'Número escolhido é menor'
+  false
+end
+
+da_boas_vindas
+numero_secreto = escolhe_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+  chute = pede_um_numero tentativa, limite_de_tentativas
+
+  break if verifica_se_acertou numero_secreto, chute
 end
